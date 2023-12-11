764b122d0c066806a98e385b9dee617c9cca232e
westkite05 <westkite23@naver.com>
2023-12-11 17:13:46 +0900

    2nd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import type { AppProps } from 'next/app'
-import { ThirdwebProvider } from '@thirdweb-dev/react'
-import { WalletContextProvider } from '../context/WalletConnectionContext'
-import { ThemeProvider } from '@mui/material'
-import theme from '../styles/theme'
-import { ResponsiveDrawer } from '../components'
-import { AuthorityContextProvider } from '../context/AuthorityContext'
+import type { AppProps } from "next/app"
+import { ThirdwebProvider } from "@thirdweb-dev/react"
+import { WalletContextProvider } from "../context/WalletConnectionContext"
+import { ThemeProvider } from "@mui/material"
+import theme from "../styles/theme"
+import { ResponsiveDrawer } from "../components"
+import { AuthorityContextProvider } from "../context/AuthorityContext"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/new-election.tsx
+++ pages/new-election.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, ChangeEvent, useState } from 'react'
+import React, { useContext, ChangeEvent, useState } from "react"
 import {
   Grid,
   Typography,
@@ -6,9 +6,9 @@ import {
   Backdrop,
   CircularProgress,
   Slide,
-} from '@mui/material'
-import WarningAmberRoundedIcon from '@mui/icons-material/WarningAmberRounded'
-import CancelIcon from '@mui/icons-material/Cancel'
+} from "@mui/material"
+import WarningAmberRoundedIcon from "@mui/icons-material/WarningAmberRounded"
+import CancelIcon from "@mui/icons-material/Cancel"
 import {
   StyledChildBox,
   StyledList,
@@ -17,15 +17,15 @@ import {
   StyledSuccessBox,
   StyledTextField,
   StyledTypography,
-} from '../styles/newElectionStyle'
-import { AuthorityContext, Election } from '../context/AuthorityContext'
+} from "../styles/newElectionStyle"
+import { AuthorityContext, Election } from "../context/AuthorityContext"
 
 function NewElection() {
   const { isBallotInitialized, isBallotLoading, initializeBallot } =
     useContext(AuthorityContext)
 
   const [electionState, setElectionState] = useState<Election>({
-    name: '',
+    name: "",
     startTime: null as unknown as number,
     endTime: null as unknown as number,
   })
@@ -58,13 +58,13 @@ function NewElection() {
 
   return (
     <>
-      <Grid sx={{ justifyContent: 'space-around', paddingTop: 8 }} container>
+      <Grid sx={{ justifyContent: "space-around", paddingTop: 8 }} container>
         <Grid item md={12}>
           <Typography
             sx={{
-              textAlign: 'center',
-              fontWeight: 'bold',
-              color: 'white',
+              textAlign: "center",
+              fontWeight: "bold",
+              color: "white",
               paddingBottom: 10,
             }}
             variant="h5"
@@ -74,49 +74,49 @@ function NewElection() {
         </Grid>
         <Grid item mx={6}>
           <StyledList>
-            <StyledListItem sx={{ listStyle: 'none' }}>
-              <WarningAmberRoundedIcon sx={{ fontSize: 70, color: 'red' }} />
+            <StyledListItem sx={{ listStyle: "none" }}>
+              <WarningAmberRoundedIcon sx={{ fontSize: 70, color: "red" }} />
               <Typography
                 variant="h4"
-                sx={{ fontWeight: 'bold', color: 'red' }}
+                sx={{ fontWeight: "bold", color: "red" }}
               >
-                Be careful
+                주의사항
               </Typography>
             </StyledListItem>
             <StyledListItem>
               <ListItemText>
-                Please be sure before Initialize Ballot
+                투표를 초기화하기 전에 반드시 확인하시기 바랍니다
               </ListItemText>
             </StyledListItem>
             <StyledListItem>
-              <ListItemText>You can't modify after confirmation.</ListItemText>
+              <ListItemText>확인후 수정이 불가합니다</ListItemText>
             </StyledListItem>
             <StyledListItem>
               <ListItemText>
-                Only an authorized person has rights to Initialize Ballot.
+                승인된 사람만이 투표용지를 초기화할 수 있는 권한이 있습니다
               </ListItemText>
             </StyledListItem>
           </StyledList>
         </Grid>
         <Grid item mx={6}>
           <StyledChildBox>
-            <StyledTypography>Election Name:</StyledTypography>
+            <StyledTypography>투표 주제</StyledTypography>
             <StyledTextField
               value={electionState.name}
               autoComplete="off"
               name="name"
               onChange={handleNameChange}
-              placeholder="Enter Election name"
+              placeholder="투표 주제를 입력하세요"
               type="text"
             />
           </StyledChildBox>
           <StyledChildBox>
             <StyledTypography>
-              Enter Election start Date & Time
+              투표 시작 날짜 및 시간을 입력하시오
             </StyledTypography>
             <StyledTextField
               name="startTime"
-              onChange={(e) => handleTimeChange(e, 'startTime')}
+              onChange={(e) => handleTimeChange(e, "startTime")}
               type="datetime-local"
               inputProps={{
                 min: new Date().toISOString().slice(0, 16),
@@ -124,9 +124,11 @@ function NewElection() {
             />
           </StyledChildBox>
           <StyledChildBox>
-            <StyledTypography>Enter Election end Date & Time</StyledTypography>
+            <StyledTypography>
+              투표 마감 날짜 및 시간을 입력하시오
+            </StyledTypography>
             <StyledTextField
-              onChange={(e) => handleTimeChange(e, 'endTime')}
+              onChange={(e) => handleTimeChange(e, "endTime")}
               name="endTime"
               type="datetime-local"
               inputProps={{
@@ -135,23 +137,23 @@ function NewElection() {
             />
           </StyledChildBox>
           <StyledSubmitBtn onClick={handleOnSubmitBtn}>
-            Initialize
+            완료?등록?
           </StyledSubmitBtn>
         </Grid>
       </Grid>
 
       <Slide direction="up" in={isBallotInitialized} mountOnEnter unmountOnExit>
         <StyledSuccessBox>
-          <Typography color={'white'}>Successful</Typography>
-          <CancelIcon sx={{ cursor: 'pointer' }} />
+          <Typography color={"white"}>Successful</Typography>
+          <CancelIcon sx={{ cursor: "pointer" }} />
         </StyledSuccessBox>
       </Slide>
 
       <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={isBallotLoading}
       >
-        <CircularProgress sx={{ color: 'gray' }} />
+        <CircularProgress sx={{ color: "gray" }} />
       </Backdrop>
     </>
   )
